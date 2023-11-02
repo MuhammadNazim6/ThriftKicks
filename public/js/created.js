f1a5c55c34eca11ce714a7b6b20f0b7f266b2db2
MuhammadNazim6 <nazimnazz66@gmail.com>
2023-11-02 21:55:59 +0530

    daily commit
    02/11/23

--- public/js/created.js
+++ public/js/created.js
@@ -6,3 +6,57 @@ document.getElementById("cartButton").addEventListener("click", function () {
 });
 
 
+//product view image cganging functionality
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const button3 = document.getElementById('button3')
+const image1 = document.getElementById('image1')
+const image2 = document.getElementById('image2')
+const image3 = document.getElementById('image3')
+
+const zimage1 = document.getElementById('zimage1')
+const zimage2 = document.getElementById('zimage2')
+const zimage3 = document.getElementById('zimage3')
+
+image2.style.display = 'none';
+image3.style.display = 'none';
+
+
+button1.addEventListener('click',function(){
+    image1.style.display = 'block';
+    image2.style.display = 'none';
+    image3.style.display = 'none';
+
+    zimage1.style.display = 'block';
+    zimage2.style.display = 'none';
+    zimage3.style.display = 'none';
+})
+button2.addEventListener('click',function(){
+    image1.style.display = 'none';
+    image2.style.display = 'block';
+    image3.style.display = 'none';
+
+    zimage1.style.display = 'none';
+    zimage2.style.display = 'block';
+    zimage3.style.display = 'none';
+})
+button3.addEventListener('click',function(){
+    image1.style.display = 'none';
+    image2.style.display = 'none';
+    image3.style.display = 'block';
+
+    zimage1.style.display = 'none';
+    zimage2.style.display = 'none';
+    zimage3.style.display = 'block';
+})
+
+
+//
+
+
+// Apply zoom effect to all elements with the 'zoomable-image' class
+// $('.zoomable-image').each(function() {
+//   var $element = $(this);
+//   var zoomInstance = $element.zoom(3);
+//   zoomInstance.setZoom(20);
+// });
