44fa5c7f0cb376fc77db2913cade58b6b5913a2f
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-01-01 10:04:04 +0530

    changes made in forgot password and change password page
    made responsive and changed email html format

--- services/mail.js
+++ services/mail.js
@@ -16,16 +16,24 @@ async function sendMail(toMail,otp,name){
     from: process.env.EMAIL,
     to: toMail,
     subject: 'OTP for Email Verification of Thrift Kicks',
-
     html: `
-        <div class="email-container">
-            <h3>Dear ${name},</h3>
-            <p>Thank you for choosing Thrift Kicks. Your OTP for email verification is: <strong>${otp}</strong>. Please use this code to complete the verification process.</p>
+        <div style="font-family: 'Arial', sans-serif; max-width: 600px; margin: 0 auto;">
+            <div style="background-color: #f8f8f8; padding: 20px; text-align: center; border-radius: 10px;">
+                <h3 style="color: #333;">Dear ${name},</h3>
+                <p style="color: #555; font-size: 16px;">Thank you for choosing Thrift Kicks. Your OTP for email verification is: </p>
+                
+                <div style="margin: 20px 0; background-color: #fff; padding: 10px; border: 1px solid #ddd; border-radius: 5px;">
+                    <p style="margin: 0; font-size: 18px; color: #333;">OTP: <span id="otpDisplay" style="font-weight: bold;">${otp}</span></p>
+                </div>
 
-            <p>Best Regards,<br/>The Thrift Kicks Team</p>
-        </div>  
+                <p style="color: #555; font-size: 16px;">Please use this code to complete the verification process.</p>
+                <p style="color: #555; font-size: 16px;">Best Regards,<br/>The Thrift Kicks Team</p>
+            </div>
+        </div> 
     `
 };
+
+
 
   try {
     const result = await transporter.sendMail(mailOptions)
@@ -47,19 +55,27 @@ async function sendForgotPassMail(toMail,otp,name){
  
    //mail content
    const mailOptions = {
-     from: process.env.EMAIL,
-     to: toMail,
-     subject: 'OTP for changing password of Thrift Kicks account',
- 
-     html: `
-         <div class="email-container">
-             <h3>Dear ${name},</h3>
-             <p>Thank you for choosing Thrift Kicks. Your OTP for resetting your password is: <strong>${otp}</strong>. Please use this code to change your password.</p>
- 
-             <p>Best Regards,<br/>The Thrift Kicks Team</p>
-         </div>  
-     `
- };
+    from: process.env.EMAIL,
+    to: toMail,
+    subject: 'OTP for changing password of Thrift Kicks account',
+    html: `
+        <div style="font-family: 'Arial', sans-serif; max-width: 600px; margin: 0 auto;">
+            <div style="background-color: #f8f8f8; padding: 20px; text-align: center; border-radius: 10px;">
+                <h3 style="color: #333;">Dear ${name},</h3>
+                <p style="color: #555; font-size: 16px;">Thank you for choosing Thrift Kicks. Your OTP for resetting your password is: </p>
+                
+                <div style="margin: 20px 0; background-color: #fff; padding: 10px; border: 1px solid #ddd; border-radius: 5px;">
+                    <p style="margin: 0; font-size: 18px; color: #333;">OTP: <span id="otpDisplay" style="font-weight: bold;">${otp}</span></p>
+                </div>
+
+                <p style="color: #555; font-size: 16px;">Please use this code to change your password.</p>
+                <p style="color: #555; font-size: 16px;">Best Regards,<br/>The Thrift Kicks Team</p>
+            </div>
+        </div>
+    `
+};
+
+
  
    try {
      const result = await transporter.sendMail(mailOptions)
