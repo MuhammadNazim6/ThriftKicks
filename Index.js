0810f9e9309d3ff4b02d2487ee67354d2a00eff6
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-07-14 18:38:58 +0530

    latest changes

--- Index.js
+++ Index.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 
 
 app.listen(process.env.PORT || 8000, () => {
-  console.log("server is Running");
+  console.log(`server is Running at port ${process.env.PORT}`);
 });
 
 
